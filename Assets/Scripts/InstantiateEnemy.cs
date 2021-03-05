9ade3a62d7283059846a51838b5f92ec9beae2d7
Megapartymen <pervi@list.ru>
2021-03-05 13:58:56 +0500

    Update InstantiateEnemy.cs
    1 убрал комментарии
    2 Сделал массив Vector3
    3 Убрал дублирующийся код

--- Assets/Scripts/InstantiateEnemy.cs
+++ Assets/Scripts/InstantiateEnemy.cs
@@ -10,17 +10,16 @@ public class InstantiateEnemy : MonoBehaviour
     [SerializeField] private GameObject _spawnPointThird;
     [SerializeField] private int _spawnPause;
 
-    private Vector3 _spawnAddressFirst;
-    private Vector3 _spawnAddressSecond;
-    private Vector3 _spawnAddressThird;
-
+    private List<Vector3> _spawnAddresses;
+    
     private void Start()
     {
-        _spawnAddressFirst = new Vector3 (_spawnPointFirst.transform.position.x, _spawnPointFirst.transform.position.y + 1, _spawnPointFirst.transform.position.z);
-        _spawnAddressSecond = new Vector3(_spawnPointSecond.transform.position.x, _spawnPointSecond.transform.position.y + 1, _spawnPointSecond.transform.position.z);
-        _spawnAddressThird = new Vector3(_spawnPointThird.transform.position.x, _spawnPointThird.transform.position.y + 1, _spawnPointThird.transform.position.z);
+        _spawnAddresses = new List<Vector3>();
+        _spawnAddresses.Add(new Vector3(_spawnPointFirst.transform.position.x, _spawnPointFirst.transform.position.y + 1, _spawnPointFirst.transform.position.z));
+        _spawnAddresses.Add(new Vector3(_spawnPointSecond.transform.position.x, _spawnPointSecond.transform.position.y + 1, _spawnPointSecond.transform.position.z));
+        _spawnAddresses.Add(new Vector3(_spawnPointThird.transform.position.x, _spawnPointThird.transform.position.y + 1, _spawnPointThird.transform.position.z));        
 
-        StartCoroutine(CircleInstantiate());
+       StartCoroutine(CircleInstantiate());
     }
 
     private void CreateEnemy(Vector3 spawnPoint)
@@ -34,41 +33,11 @@ public class InstantiateEnemy : MonoBehaviour
 
         while (onInstantiate)
         {
-            CreateEnemy(_spawnAddressFirst);
-            yield return new WaitForSeconds(_spawnPause);
-            CreateEnemy(_spawnAddressSecond);
-            yield return new WaitForSeconds(_spawnPause);
-            CreateEnemy(_spawnAddressThird);
-            yield return new WaitForSeconds(_spawnPause);
+            foreach (Vector3 spawnAddress in _spawnAddresses)
+            {
+                CreateEnemy(spawnAddress);
+                yield return new WaitForSeconds(_spawnPause);
+            }            
         }
-    }
-
-
-
-
-
-
-
-
-    //private void CircleInstantiate()
-    //{
-    //    bool off = true;
-        
-    //    while (off)
-    //    {
-    //        CreateEnemy(_spawnPointFirst);
-    //        StartCoroutine(WaitNextEnemy());
-    //        CreateEnemy(_spawnPointSecond);
-    //        StartCoroutine(WaitNextEnemy());
-    //        CreateEnemy(_spawnPointThird);
-    //        StartCoroutine(WaitNextEnemy());
-
-    //        off = false;
-    //    }
-    //}
-
-    //private IEnumerator WaitNextEnemy()
-    //{
-    //    yield return new WaitForSeconds(2);
-    //}
+    }    
 }
